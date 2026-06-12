b245d5366d6cbd92e9367a55fee57341a8431d8a
Alexs1313 <o.salata@o-salata.local>
2026-06-12 17:04:44 +0300

    upd

--- android/app/src/main/java/com/cascadesgueststay/MainActivity.kt
+++ android/app/src/main/java/com/cascadesgueststay/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.cascadesgueststay
+package com.cascadesguest.guide
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/cascadesgueststay/MainApplication.kt
+++ android/app/src/main/java/com/cascadesgueststay/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.cascadesgueststay
+package com.cascadesguest.guide
 
 import android.app.Application
 import com.facebook.react.PackageList
